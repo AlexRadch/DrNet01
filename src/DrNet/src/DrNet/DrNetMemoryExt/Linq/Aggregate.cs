45173aac5f0f9a9755bad4b04de1cbb39f91f029
Alexander Radchenko <radchenkosasha@gmail.com>
2018-10-17 15:26:15 +0700

    Added DrNetMemoryExt Aggregate

---
+++ src/DrNet/src/DrNet/DrNetMemoryExt/Linq/Aggregate.cs
@@ -0,0 +1,93 @@
+﻿using DrNet.Unsafe;
+using System;
+
+namespace DrNet
+{
+    public static partial class DrNetMemoryExt
+    {
+        public static TSource Aggregate<TSource>(this Span<TSource> span, Func<TSource, TSource, TSource> func)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            int count = span.Length;
+            if (count <= 0)
+                throw new InvalidOperationException();
+
+            ref readonly TSource current = ref span[0];
+            TSource result = current;
+            while (--count > 0)
+            {
+                UnsafeIn.Add(current, 1);
+                result = func(result, current);
+            }
+            return result;
+        }
+
+        public static TSource Aggregate<TSource>(this ReadOnlySpan<TSource> span, Func<TSource, TSource, TSource> func)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            int count = span.Length;
+            if (count <= 0)
+                throw new InvalidOperationException();
+
+            ref readonly TSource current = ref span[0];
+            TSource result = current;
+            while (--count > 0)
+            {
+                UnsafeIn.Add(current, 1);
+                result = func(result, current);
+            }
+            return result;
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this Span<TSource> span, TAccumulate seed,
+            Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            TAccumulate result = seed;
+            int count = span.Length;
+            if (count > 0)
+            {
+                ref readonly TSource current = ref DrNetMarshal.GetReference(span);
+                for (;;)
+                {
+                    result = func(result, current);
+                    if (--count <= 0)
+                        break;
+                    UnsafeIn.Add(current, 1);
+                }
+            }
+            return result;
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this ReadOnlySpan<TSource> span, TAccumulate seed,
+            Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            TAccumulate result = seed;
+            int count = span.Length;
+            if (count > 0)
+            {
+                ref readonly TSource current = ref DrNetMarshal.GetReference(span);
+                for (;;)
+                {
+                    result = func(result, current);
+                    if (--count <= 0)
+                        break;
+                    UnsafeIn.Add(current, 1);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/DrNet/src/DrNet/DrNetMemoryExt/Linq/TrimTo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DrNet
+{
+    public static partial class DrNetMemoryExt
+    {
+        public static Span<TSource> TrimStartTo<TSource>(this Span<TSource> span, int start)
+        {
+            if (start > 0)
+                return span.Slice(start);
+            if (start == 0)
+                return span;
+            return span.Slice(span.Length, 0);
+        }
+
+        public static Span<TSource> TrimEndTo<TSource>(this Span<TSource> span, int end)
+        {
+            if (end > 0)
+                return span.Slice(0, end);
+            return span.Slice(0, 0);
+        }
+
+        public static Span<TSource> TrimStartEndTo<TSource>(this Span<TSource> span, int start, int end)
+        {
+            if (start > 0)
+            {
+                if (end <= start)
+                    return span.Slice(start, 0);
+                return span.Slice(start, end - start);
+            }
+            if (start == 0)
+            {
+                if (end > 0)
+                    return span.Slice(0, end);
+                return span.Slice(0, 0);
+            }
+            return span.Slice(span.Length, 0);
+        }
+    }
+}
